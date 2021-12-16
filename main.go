95f50bb3b8fbc8b1b5e68e0f7edc226b5169d388
Antonio S. Dromundo <sebastiandromundo@outlook.com>
2021-12-15 23:40:44 -0600

    Some tests examples for profiling

--- main.go
+++ main.go
@@ -10,3 +10,24 @@ func GetMax(x, y int) int {
 	}
 	return y
 }
+
+func Fibonacci(n int) int {
+	if n <= 1 {
+		return n
+	}
+	return (Fibonacci(n-1) + Fibonacci(n-2))
+}
+
+func UseTailFibonacci(n int) int {
+	return TailFibonacci(n, 0, 1)
+}
+
+func TailFibonacci(n, a, b int) int {
+	if n == 0 {
+		return a
+	} else if n == 1 {
+		return b
+	} else {
+		return TailFibonacci(n-1, b, a+b)
+	}
+}
--- main_test.go
+++ main_test.go
@@ -1,6 +1,8 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	total := Sum(5, 5)
@@ -26,5 +28,101 @@ func TestTableSum(t *testing.T) {
 		if total != item.n {
 			t.Errorf("Sum was incorrect, got %d expected %d", total, item.n)
 		}
+	}
+}
+
+func TestGetMax(t *testing.T) {
+	type args struct {
+		x int
+		y int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "Positivos",
+			args: args{1, 1000},
+			want: 1000,
+		},
+		{
+			name: "Negativos",
+			args: args{0, -1000},
+			want: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := GetMax(tt.args.x, tt.args.y); got != tt.want {
+				t.Errorf("GetMax() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestFibonacci(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "Max Fib in int64",
+			args: args{50},
+			want: 12586269025,
+		},
+		{
+			name: "Fib(50)",
+			args: args{1},
+			want: 1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Fibonacci(tt.args.n); got != tt.want {
+				t.Errorf("Fibonacci() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestUseTailFibonacci(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "Base case 0",
+			args: args{0},
+			want: 0,
+		},
+		{
+			name: "Base case 1",
+			args: args{1},
+			want: 1,
+		},
+		{
+			name: "Max case 50",
+			args: args{50},
+			want: 12586269025,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := UseTailFibonacci(tt.args.n); got != tt.want {
+				t.Errorf("UseTailFibonacci() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
